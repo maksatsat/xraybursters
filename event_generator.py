6e7b925a74e3ba3d0c5f06710559e98e00a2688f
maksatsat <maksatsatybaldiev@gmail.com>
2020-10-22 14:31:47 +0600

    Converting datetime

--- event_generator.py
+++ event_generator.py
@@ -6,7 +6,12 @@ class Events:
     def __init__(self, m, intervals):
         self.m = m  # Число событий в единицу времени
         self.intervals = intervals
-        self.events = Events.generate_events(self.m, self.intervals)
+        self.start_time, self.intervals_in_seconds = Events.dates_to_seconds(
+            self.intervals)
+        self.events_in_seconds = Events.generate_events(
+            self.m, self.intervals_in_seconds)
+        self.events = Events.seconds_to_dates(
+            self.start_time, self.events_in_seconds)
 
     def generate_event(m, interval):
         start = interval[0]
@@ -31,3 +36,24 @@ class Events:
             event = Events.generate_event(m, interval)
             events += event
         return events
+
+    def dates_to_seconds(intervals):
+        import datetime
+        start = datetime.datetime.strptime(
+            intervals[0][0], '%Y-%m-%d %H:%M:%S.%f')
+        intervals_in_seconds = []
+        for interval in intervals:
+            t_start = datetime.datetime.strptime(
+                interval[0], '%Y-%m-%d %H:%M:%S.%f')-start
+            t_end = datetime.datetime.strptime(
+                interval[1], '%Y-%m-%d %H:%M:%S.%f')-start
+            intervals_in_seconds.append(
+                [t_start.total_seconds(), t_end.total_seconds()])
+        return start, intervals_in_seconds
+
+    def seconds_to_dates(start_time, events_in_seconds):
+        import datetime
+        event_times = []
+        for event in events_in_seconds:
+            event_times.append(datetime.timedelta(seconds=event)+start_time)
+        return event_times
